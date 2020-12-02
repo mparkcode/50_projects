c9eca875979304eda1e7453a194ff5b340b785c4
Michael Park <mparkcode@gmail.com>
2020-12-03 08:18:03 +0900

    01 expanding cards

---
+++ 01_expanding_cards/script.js
@@ -0,0 +1,18 @@
+console.log('hi')
+
+const panels = document.querySelectorAll('.panel');
+
+console.log(panels)
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  })
+});
+
+const removeActiveClasses = () => {
+  panels.forEach(panel => {
+    panel.classList.remove('active')
+  })
+}
