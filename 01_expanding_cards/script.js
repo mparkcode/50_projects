cd423200dbf68f882f9b4392c837e2d9c65dce6e
Michael Park <mparkcode@gmail.com>
2020-12-09 07:57:26 +0900

    changed clickevent to mouse over on expanding cards

--- 01_expanding_cards/script.js
+++ 01_expanding_cards/script.js
@@ -1,7 +1,7 @@
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
-  panel.addEventListener('click', () => {
+  panel.addEventListener('mouseenter', () => {
     removeActiveClasses();
     panel.classList.add('active');
   })
