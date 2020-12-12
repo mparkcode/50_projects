5b829daec673128411b5f16708d0ea0fe1d2e4a9
Michael Park <mparkcode@gmail.com>
2020-12-12 09:42:54 +0900

    10_dad_jokes

---
+++ 10_dad_jokes/script.js
@@ -0,0 +1,32 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+// const generateJoke = () => {
+//   const config = {
+//     headers: {
+//       'Accept': 'application/json'
+//     }
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then(res => res.json())
+//     .then(data => );
+
+// }
+
+const generateJoke = async () => {
+  const config = {
+    headers: {
+      'Accept': 'application/json'
+    }
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config);
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+
+}
+
+generateJoke();
+
+jokeBtn.addEventListener('click', generateJoke);
